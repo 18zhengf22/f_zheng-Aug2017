54b4905bbba8be9a6424989ff3b154486a1fd845
18zhengf22 <18zhengf22@millshighsch.org>
2017-08-21 09:12:37 -0700

    Hello World

--- Hello_World/src/Hello_World.java
+++ Hello_World/src/Hello_World.java
@@ -3,6 +3,8 @@ public class Hello_World {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Hello World! From Frank");
+	//Display the string
 
 	}
 
