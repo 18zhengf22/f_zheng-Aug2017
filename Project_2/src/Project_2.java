de5ff00bbf79945bc08d33dea31709e7ae0240c6
18zhengf22 <18zhengf22@millshighsch.org>
2017-08-23 11:27:53 -0700

    Project_2

---
+++ Project_2/src/Project_2.java
@@ -0,0 +1,14 @@
+//Frank Zheng
+//August 23, 2017
+//Project 2 -- Use print and println
+public class Project_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("hi There!");
+		System.out.println("hi There!");
+		System.out.print("Hi There!");
+
+	}
+
+}
