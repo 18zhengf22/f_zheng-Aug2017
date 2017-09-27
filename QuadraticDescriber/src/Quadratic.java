e76ecc363a241345858a3307b9d2ca6d1cf6d8bd
18zhengf22 <18zhengf22@millshighsch.org>
2017-09-27 11:31:03 -0700

    Started Part 5.

---
+++ QuadraticDescriber/src/Quadratic.java
@@ -0,0 +1,8 @@
+/*Frank Zheng
+ * September 27, 2017
+ * Part of QuadraticDescriber
+ * It is responsible for all the calculations.
+ */
+public class Quadratic {
+
+}
---
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -0,0 +1,13 @@
+/*Frank Zheng
+ * September 27, 2017
+ * Part of QuadraticDescriber
+ * It will handle interactions with the user.
+ */
+public class QuadraticClient {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
