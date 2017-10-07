e56063073af3440bb9cdcb8a6ca4488108932e58
18zhengf22 <18zhengf22@millshighsch.org>
2017-10-07 15:14:22 -0700

    fixed after resolve

--- Collaborative Programming Exercise/src/ProcessingNumbers.java
+++ Collaborative Programming Exercise/src/ProcessingNumbers.java
@@ -1,5 +1,5 @@
 /*Frank Zheng
- * October 4, 2017
+ * October 5, 2017
  * This is the final step for the Collaborative Programming Exercise.
  * Prints min, max, sum of evens, and largest even.
  */
@@ -33,16 +33,16 @@ public class ProcessingNumbers {
 		String finished = userInput.next();
 		if(finished.equals("yes")) {
 			done = true;
-			if(maxEven %2 != 0) {
+			if(maxEven % 2 != 0) {
 				System.out.println("No even numbers.");
 			}
 		}
 	} while(done == false);
-	System.out.println("This is your min value: " + min);
-	System.out.println("This is your max value: " + max);
-	System.out.println("This is your even sum: " + sum);
+	System.out.println("This is your minimum: " + min);
+	System.out.println("This is your maximum: " + max);
+	System.out.println("This is the sum of your even numbers: " + sum);
 	if(maxEven %2 == 0) {
-	System.out.println("This is your max even value: " + maxEven);
+	System.out.println("This is your maximum even: " + maxEven);
 	}
 	}
 }
