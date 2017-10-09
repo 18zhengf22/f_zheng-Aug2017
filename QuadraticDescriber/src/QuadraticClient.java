7d190e35b8f1b62aa489d1a506313cc694ff3b00
18zhengf22 <18zhengf22@millshighsch.org>
2017-10-09 01:12:32 -0700

    Started Part 5.

--- QuadraticDescriber/src/QuadraticClient.java
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -3,10 +3,24 @@
  * Part of QuadraticDescriber
  * It will handle interactions with the user.
  */
+import java.util.Scanner;
 public class QuadraticClient {
 
 	public static void main(String[] args) {
-		
+		Scanner userInput = new Scanner(System.in);
+		do {
+			System.out.println("Provide values for coefficients a, b, and c");
+			System.out.println();
+			System.out.println("a: ");
+			double a = userInput.nextDouble();
+			System.out.println("b: ");
+			double b = userInput.nextDouble();
+			System.out.println("c: ");
+			double c = userInput.nextDouble();
+			System.out.println();
+			quadrDescriber(a, b, c);
+			System.out.println(description);
+		} while (done == false);
 
 	}
 
