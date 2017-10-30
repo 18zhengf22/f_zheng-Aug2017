47b06662a7d3ce1e5f385e629e91c2f220fdfefc
18zhengf22 <18zhengf22@millshighsch.org>
2017-10-30 09:52:30 -0700

    Start Array Mini-Lab.

--- SplitString/src/Split.java
+++ SplitString/src/Split.java
@@ -12,9 +12,41 @@ public class Split {
 		* use String.split to split up the sandwich by the word "bread" and return what's in the middle of the sandwich and ignores what's on the outside
 		* What if it's a fancy sandwich with multiple pieces of bread?
 		*/
-		sandwich.split("bread");
-		String filling = 
+		String[] swBread = sandwich.split("bread");
+		String filling = "";
+		for (int i = 1; i < swBread.length - 1; i++) {
+			filling += swBread[i];
+		}
 		return filling;
 	}
-
 }
+
+	//Your task Part 2:
+	/*Write a method that take in a string like "apples pineapples bread lettus tomato bacon mayo ham bread cheese" describing a sandwich
+	* use String.split to split up the sandwich at the spaces, " ", and return what's in the middle of the sandwich and ignores what's on the outside
+	* Again, what if it's a fancy sandwich with multiple pieces of bread?
+	*/
+	public static String splitSwSpaces(String sandwich) {
+		String[] items = sandwich.split(" ");
+		int firstBread = 0;
+		for (int i = 0; i < items.length; i++) {
+			if (items[i].equals("bread")) {
+				firstBread = i;
+				break;
+			}
+		}
+		int lastBread = 0;
+		for(int i = items.length - 1; i > -1; i--) {
+			if(items[i].equals("bread")) {
+				lastBread = i;
+				break;
+			}
+		}
+		String contents = "";
+		for(int i = firstBread + 1; i < lastBread; i++) {
+			contents += items[i] + " ";
+		}
+		return contents;
+	}
+}
+
