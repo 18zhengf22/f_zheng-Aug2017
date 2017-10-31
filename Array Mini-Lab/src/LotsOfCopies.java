809cfe750c3362b1d9baba620015e06a49cf8a85
18zhengf22 <18zhengf22@millshighsch.org>
2017-10-30 22:22:37 -0700

    minilab and split (kinda)

--- Array Mini-Lab/src/LotsOfCopies.java
+++ Array Mini-Lab/src/LotsOfCopies.java
@@ -5,10 +5,12 @@ public class LotsOfCopies {
 		int num = 7;
 		String strMain = "APCS";
 		int[] arrMain = {1, 2, 3, 4, 5};
-
+		changeMe(num, strMain, arrMain);
+		System.out.println("main array first number: " + arrMain[0]);
 	}
 	public static void changeMe (int x, String str, int[] arr) {
-		
+		arr[0] = x;
+		System.out.println("changed array first number: " + arr[0]);
 	}
 
 }
--- SplitString/src/Split.java
+++ SplitString/src/Split.java
@@ -1,32 +1,46 @@
 import java.util.Arrays;
 
 public class Split {
-
+	
+	// Your task Part 0
+	//String.split();
+	//It's a method that acts on a string, <StringName>.split(<String sp>);
+	//Where sp is the string where the string splits
+	//And it returns an array
+	// Example: "I like apples!".split(" "); 
+//		it will split at spaces and return an array of ["I","like","apples!"]
+	// Example 2: "I really like really red apples".split("really")
+//		it will split at the word "really" and return an array of ["I "," like "," red apples!"]
+	//play around with String.split! 
+	//What happens if you "I reallyreally likeapples".split("really") ?
 	public static void main(String[] args) {
 		System.out.println(Arrays.toString("I like apples!".split(" ")));
 		System.out.println(Arrays.toString("I really like really red apples".split("really")));
+		System.out.println(Arrays.toString("I reallyreally likeapples".split("really")));
+		System.out.println(Split.getTheFilling("breadapplespineapplesbreadlettustomatobaconmayohambreadcheese"));
+		System.out.println(Split.splitAtSpaces("apples pineapples bread lettus bread tomato bacon mayo ham bread cheese"));
 	}
-	public static String getTheFilling (String sandwich) {
+
 		//Your task Part 1:
 		/*Write a method that take in a string like "applespineapplesbreadlettustomatobaconmayohambreadcheese" describing a sandwich
 		* use String.split to split up the sandwich by the word "bread" and return what's in the middle of the sandwich and ignores what's on the outside
 		* What if it's a fancy sandwich with multiple pieces of bread?
 		*/
-		String[] swBread = sandwich.split("bread");
+	public static String getTheFilling (String sandwich) {
+		String[] theBread = sandwich.split("bread");
 		String filling = "";
-		for (int i = 1; i < swBread.length - 1; i++) {
-			filling += swBread[i];
+		for (int i = 1; i < theBread.length - 1; i++) {
+			filling += theBread[i];
 		}
 		return filling;
 	}
-}
 
 	//Your task Part 2:
 	/*Write a method that take in a string like "apples pineapples bread lettus tomato bacon mayo ham bread cheese" describing a sandwich
 	* use String.split to split up the sandwich at the spaces, " ", and return what's in the middle of the sandwich and ignores what's on the outside
 	* Again, what if it's a fancy sandwich with multiple pieces of bread?
 	*/
-	public static String splitSwSpaces(String sandwich) {
+	public static String splitAtSpaces (String sandwich) {
 		String[] items = sandwich.split(" ");
 		int firstBread = 0;
 		for (int i = 0; i < items.length; i++) {
@@ -36,17 +50,17 @@ public class Split {
 			}
 		}
 		int lastBread = 0;
-		for(int i = items.length - 1; i > -1; i--) {
+		for (int i = items.length - 1; i > -1; i--) {
 			if(items[i].equals("bread")) {
 				lastBread = i;
 				break;
 			}
 		}
-		String contents = "";
-		for(int i = firstBread + 1; i < lastBread; i++) {
-			contents += items[i] + " ";
+		String filling = "";
+		for (int i = firstBread + 1; i < lastBread; i++) {
+			filling += items[i] + " ";
 		}
-		return contents;
+		return filling;
 	}
 }
 
