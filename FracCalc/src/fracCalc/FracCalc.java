e6a6defb397c60fa06e10cb5769c06824ff5faaf
18zhengf22 <18zhengf22@millshighsch.org>
2017-11-19 17:56:09 -0800

    Started Checkpoint 3.

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -4,17 +4,16 @@
  */
 package fracCalc;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class FracCalc {
 
     public static void main(String[] args) {
-    	Scanner userInput = new Scanner(System.in);
-    	while (userInput.next() != "quit") {
-    		String input = userInput.nextLine();
-    		produceAnswer(input);
-    		System.out.println(produceAnswer(input));
-    	}
+    		Scanner userInput = new Scanner(System.in);
+    		while (!userInput.next().equals("quit")) {
+    			String input = userInput.nextLine();
+    			System.out.println(produceAnswer(input));
+    		}
         // TODO: Read the input from the user and call produceAnswer with an equation
 
     }
@@ -28,44 +27,65 @@ public class FracCalc {
     // The function should return the result of the fraction after it has been calculated
     //      e.g. return ==> "1_1/4"
     public static String produceAnswer(String input) {
-    	String[] array = input.split(" ");
-    	String firstOperand = array[0];
-    	String operator = array[1];
-    	String secondOperand = array[2];
-    	splitOperand(firstOperand);
-    	splitOperand(secondOperand);
-    	String result = "whole:" + splitOperand(secondOperand)[0] + " numerator:" + splitOperand(secondOperand)[1] + " denominator:" + splitOperand(secondOperand)[2];
-    	return result;
+    		String[] arrSplit = input.split(" ");
+    		String firstOperand = arrSplit[0];
+    		String operator = arrSplit[1];
+    		String secondOperand = arrSplit[2];
+    		int[] arrFirstOperand = splitOperand(firstOperand);
+    		int[] arrSecondOperand = splitOperand(secondOperand);
+    		if (operator.equals("+") || operator.equals("-")) {
+    			return addSubtract(arrFirstOperand, arrSecondOperand, operator);
+    		} else {
+    			return "multiplyDivide";
+    		}
     }
         // TODO: Implement this function to produce the solution to the input
         
         //return "";
-    public static String[] splitOperand(String operand) {
-    	String whole = "";
-    	String numer = "";
-    	String denom = "";
-    	String fraction = "";
-    	String[] arrOperand = operand.split("_");
-    	if (operand.contains("_")) {
-    		whole = arrOperand[0];
-    		fraction = arrOperand[1];
-    	} else if (operand.contains("/")) {
-    		whole = "0";
-    		fraction = arrOperand[0];
-    	} else {
-    		whole = operand;
-    		numer = "0";
-    		denom = "1";
-    	}
-    	if (!fraction.equals("")) {
-    		String[] arrFraction = fraction.split("/");
-    		numer = arrFraction[0];
-    		denom = arrFraction[1];
-    	}
-    	String[] arrComponents = {whole, numer, denom};
-    	return arrComponents;
+    public static int[] splitOperand(String operand) {
+    		int whole = 0;
+    		int numer = 0;
+    		int denom = 1;
+    		if (operand.contains("_")) {
+    			whole = Integer.parseInt(operand.split("_")[0]);
+    			numer = Integer.parseInt(operand.split("_")[1].split("/")[0]);
+    			denom = Integer.parseInt(operand.split("/")[1]);
+    		} else if (operand.contains("/")) {
+    			numer = Integer.parseInt(operand.split("/")[0]);
+    			denom = Integer.parseInt(operand.split("/")[1]);
+    		} else {
+    			whole = Integer.parseInt(operand);
+    		}
+    		int[] arrParsedInt = {whole, numer, denom};
+    		return arrParsedInt;
     }
 
     // TODO: Fill in the space below with any helper methods that you think you will need
-    
+    public static String addSubtract(int[] firstOperand, int[] secondOperand, String operator) {
+    		int[] firstImproper = toImproper(firstOperand);
+    		int[] secondImproper = toImproper(secondOperand);
+    		int firstDenom = firstImproper[1];
+    		firstImproper[0] *= secondImproper[1];
+    		firstImproper[1] *= secondImproper[1];
+    		secondImproper[0] *= firstDenom;
+    		secondImproper[1] *= firstDenom;
+    		int answerNumer = 0;
+    		int answerDenom = firstImproper[1];
+    		String answer = "";
+    		if (operator.equals("+")) {
+    			answerNumer = firstImproper[0] + secondImproper[0];
+    		} else {
+    			answerNumer = firstImproper[0] - secondImproper[0];
+    		}
+    		answer = answerNumer + "/" + answerDenom;
+    		return answer;
+    }
+    public static int[] toImproper (int[] orig) {
+		int[] improper = new int[2];
+		int numer = orig[2] * orig[0] + orig[1];
+		int denom = orig[2];
+		improper[0] = numer;
+		improper[1] = denom;
+		return improper;
+    }
 }
