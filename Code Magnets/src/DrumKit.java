35f49d87971c6f90ad776ee3fbfc7c661ed8dcdb
18zhengf22 <18zhengf22@millshighsch.org>
2017-11-29 10:51:08 -0800

    Finished Code Magnets.

---
+++ Code Magnets/src/DrumKit.java
@@ -0,0 +1,14 @@
+/*Frank Zheng
+ * November 29, 2017
+ * Part of Code Magnets
+ */
+class DrumKit {
+	boolean topHat = true;
+	boolean snare = true;
+	void playSnare() {
+		System.out.println("bang bang ba-bang");
+	}
+	void playTopHat() {
+		System.out.println("ding ding da-ding");
+	}
+}
