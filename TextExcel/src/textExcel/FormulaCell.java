3b30e3870255fdaec99f933be235bb79a86c7746
18zhengf22 <18zhengf22@millshighsch.org>
2018-04-09 09:51:59 -0700

    Working on Checkpoint 5.

--- TextExcel/src/textExcel/FormulaCell.java
+++ TextExcel/src/textExcel/FormulaCell.java
@@ -29,12 +29,20 @@ public class FormulaCell extends RealCell {
 	public double getDoubleValue() {
 		String formula = input.substring(2, input.length() - 2);
 		String[] arrFormula = formula.split(" ");
-		ArrayList<Double> numbers = new ArrayList<Double>();
-		ArrayList<String> operators = new ArrayList<String>();
+		ArrayList<String> operands = new ArrayList<String>(); //ArrayList to store operands (numbers, cell references)
+		ArrayList<String> operators = new ArrayList<String>(); //ArrayList to store operators (+, -, *, /)
+		ArrayList<Double> numbers = new ArrayList<Double>(); //ArrayList to store numeric value of operands
 		for (int i = 0; i < arrFormula.length; i += 2) {
-			numbers.add(Double.parseDouble(arrFormula[i]));
+			operands.add(arrFormula[i]);
 			if (i < arrFormula.length - 1) {
 				operators.add(arrFormula[i + 1]);
+			}
+		}
+		for (int i = 0; i < operands.size(); i++) {
+			if (Character.isLetter(operands.get(i).charAt(0))) {
+				//look at the thing and make location of it and then inspectCell
+			} else {
+				numbers.add(Double.parseDouble(operands.get(i)));
 			}
 		}
 		double temp = numbers.get(0);
